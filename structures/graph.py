227c0189b132fe2f16164362a0c1fe0ad06d850c
tumiikk <izzaturifah230@gmail.com>
2026-05-30 01:19:58 +0700

    Fix : improve fligh service algorithms

--- structures/graph.py
+++ structures/graph.py
@@ -1,13 +1,48 @@
+
+PAYMENT_FILE = "data/payment.json"
+
+from collections import deque
+from collections import deque
+from utils.file_handler import load_json
+
+# PAYMENT_FILE = "payment.json"
 class Graph:
     def __init__(self):
         self.graph = {}
+        # ambil data json
+        data = load_json(PAYMENT_FILE)
+        flights = data["flights"]
+        # otomatis isi graph
+        for flight in flights.values():
 
-    def add_edge(self, u, v):
-        if u not in self.graph:
-            self.graph[u] = []
+            asal = flight["asal"]
+            tujuan = flight["tujuan"]
 
-        self.graph[u].append(v)
+            self.add_edge(asal, tujuan)
 
-    def show(self):
-        for key, value in self.graph.items():
-            print(key, "->", value)
+    # tambah jalur
+    def add_edge(self, asal, tujuan):
+        if asal not in self.graph:
+            self.graph[asal] = []
+        self.graph[asal].append(tujuan)
+
+    # BFS
+    def bfs(self, start, goal):
+
+        queue = deque([[start]])
+        visited = set()
+
+        while queue:
+            path = queue.popleft()
+            kota = path[-1]
+            if kota == goal:
+                return path
+            if kota not in visited:
+                visited.add(kota)
+                for tetangga in self.graph.get(kota, []):
+                    jalur_baru = list(path)
+                    jalur_baru.append(tetangga)
+                    queue.append(jalur_baru)
+
+        return None
+    
